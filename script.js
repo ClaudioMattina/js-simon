b62be0a537fff7cf7f3aadff5d5a0762a29e00d5
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-03-08 16:07:37 +0100

    comando e task

---
+++ script.js
@@ -0,0 +1,16 @@
+/* Descrizione:
+Visualizzare in pagina 5 numeri casuali.
+Da lì parte un timer di 30 secondi.
+Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
+
+
+/* prende di riferimento un elemento nell'html */
+/* creare una funzione che stampi numeri casuali */
+/* creare un ciclo for che ne stampi 5 di numeri casuali */
+/* i numeri generati dovranno sparire dopo 30 secondi */
+/* creare 5 promt dove l'utente inserisce i numeri */
+/* creare un array che paragoni i numeri dell'utente con quelli generati randomicamente */
+/* stampare il risultato di quanti numeri sono stati azzeccati dall'utente */
+
+
